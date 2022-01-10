6adbba2d6cab03a5f816290f4af8a4ee4be8d01b
Rob Donnelly <rfdonnelly@gmail.com>
2022-01-10 10:19:22 -0800

    feat: add ability to uses files or pipes for input/output YAML

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,24 @@ enum OutputType {
     YAML,
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+enum PipeFile {
+    Pipe,
+    File(String),
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+struct ParsePipeFileError;
+
+impl From<&str> for PipeFile {
+    fn from(s: &str) -> Self {
+        match s {
+            "-" => PipeFile::Pipe,
+            _ => PipeFile::File(s.to_string()),
+        }
+    }
+}
+
 #[derive(Parser)]
 #[clap(about, version, author)]
 struct Args {
@@ -33,18 +51,31 @@ struct Args {
     #[clap(arg_enum, short, long, default_value="gcal")]
     output: OutputType,
 
+    /// The name of the input file to use for the yaml input.
+    #[clap(parse(from_str), long, default_value="-")]
+    input_file: PipeFile,
+    /// The name of the output file to use for the yaml output.
+    #[clap(parse(from_str), long, default_value="-")]
+    output_file: PipeFile,
+
+    /// Username for the SCMA website (https://rockclimbing.org).
     #[clap(short, long, default_value="", env="SCMA_USERNAME")]
     username: String,
+    /// Password for the SCMA website (https://rockclimbing.org).
     #[clap(short, long, default_value="", env="SCMA_PASSWORD")]
     password: String,
 
-    #[clap(short, long, default_value="-")]
-    file: String,
-
+    /// The name of the Google Calendar to sync to.
     #[clap(short, long, default_value="SCMA Test")]
     calendar: String,
+    /// The client secret JSON is downloaded by the user from the Google API console
+    /// (https://console.developers.google.com).
+    ///
+    /// This file contains JSON like '{"installed":{"client_id": ... }}'.
     #[clap(long, default_value="client_secret.json", env="GCAL_CLIENT_SECRET_PATH")]
     client_secret_json_path: String,
+    /// The token JSON file is created, written, and read by the application to persist the
+    /// authentication token.
     #[clap(long, default_value="token.json", env="GCAL_OAUTH_TOKEN_JSON_PATH")]
     oauth_token_json_path: String,
 }
@@ -62,8 +93,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .await?
         }
         InputType::YAML => {
-            info!(%args.file, "Loading events from file");
-            let events_yaml = std::fs::read_to_string(&args.file)?;
+            info!(?args.input_file, "Loading events from file");
+            let events_yaml = match args.input_file {
+                PipeFile::Pipe => todo!(),
+                PipeFile::File(path) => std::fs::read_to_string(&path)?,
+            };
             serde_yaml::from_str(&events_yaml)?
         }
     };
@@ -78,7 +112,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             ).write(&events).await?;
         }
         OutputType::YAML => {
-            println!("{}", serde_yaml::to_string(&events)?);
+            match args.output_file {
+                PipeFile::Pipe => println!("{}", serde_yaml::to_string(&events)?),
+                PipeFile::File(_) => todo!(),
+            }
         }
     }
 
