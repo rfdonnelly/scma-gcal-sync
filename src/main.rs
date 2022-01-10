93c23a515ce7a3c1928714842baa494e50be814a
Rob Donnelly <rfdonnelly@gmail.com>
2022-01-10 00:47:09 -0800

    initial gcal output

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
 mod input;
 mod model;
+mod output;
 
 use input::Web;
+use model::Event;
+use output::GCal;
 
 use clap::Parser;
+use tracing::info;
 use tracing_subscriber;
 
 #[derive(Parser, Debug)]
 #[clap(about, version, author)]
 struct Args {
-    #[clap(short, long, env="SCMA_USERNAME")]
+    #[clap(short, long, default_value="", env="SCMA_USERNAME")]
     username: String,
-    #[clap(short, long, env="SCMA_PASSWORD")]
+    #[clap(short, long, default_value="", env="SCMA_PASSWORD")]
     password: String,
+
+    #[clap(short, long, default_value="-")]
+    file: String,
+
+    #[clap(short, long, default_value="SCMA Test")]
+    calendar: String,
+    #[clap(long, default_value="client_secret.json", env="GCAL_CLIENT_SECRET_PATH")]
+    client_secret_json_path: String,
+    #[clap(long, default_value="token.json", env="GCAL_OAUTH_TOKEN_JSON_PATH")]
+    oauth_token_json_path: String,
 }
 
 #[tokio::main]
@@ -21,11 +35,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args = Args::parse();
 
-    let events = Web::new(&args.username, &args.password)
-        .read()
-        .await?;
+    // let events = Web::new(&args.username, &args.password)
+    //     .read()
+    //     .await?;
 
-    println!("{}", serde_yaml::to_string(&events)?);
+    // println!("{}", serde_yaml::to_string(&events)?);
+
+    info!(%args.file, "Loading events from file");
+    let events = std::fs::read_to_string(&args.file)?;
+    let events: Vec<Event> = serde_yaml::from_str(&events)?;
+    info!("Querying Google Calendar");
+    GCal::new(
+        &args.calendar,
+        &args.client_secret_json_path,
+        &args.oauth_token_json_path,
+    ).write(&events).await?;
 
     Ok(())
 }
--- src/model.rs
+++ src/model.rs
@@ -6,26 +6,30 @@ pub struct Event {
     pub id: String,
     pub title: String,
     pub url: String,
-    #[serde(rename(deserialize = "date"))]
+    // SCMA JSON uses "date"
+    #[serde(alias = "date")]
     pub start_date: NaiveDate,
     pub end_date: NaiveDate,
-    #[serde(rename(deserialize = "venue"))]
+    // SCMA JSON uses "venue"
+    #[serde(alias = "venue")]
     pub location: String,
     pub description: String,
-    #[serde(skip_deserializing)]
+    // Not present in SCMA JSON
+    #[serde(default)]
     pub comments: Option<Vec<Comment>>,
-    #[serde(skip_deserializing)]
+    // Not present in SCMA JSON
+    #[serde(default)]
     pub attendees: Option<Vec<Attendee>>,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Comment {
     pub author: String,
     pub date: DateTime<Local>,
     pub text: String,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Attendee {
     pub name: String,
     pub count: u8,
