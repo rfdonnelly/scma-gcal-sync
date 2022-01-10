c99eec1728c41b7e68defd45720b98ffdf4800e1
Rob Donnelly <rfdonnelly@gmail.com>
2022-01-10 09:47:02 -0800

    feat: add options to specify input/output formats

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,36 @@ mod model;
 mod output;
 
 use input::Web;
-use model::Event;
 use output::GCal;
 
-use clap::Parser;
+use clap::{
+    ArgEnum,
+    Parser,
+};
 use tracing::info;
 use tracing_subscriber;
 
-#[derive(Parser, Debug)]
+#[derive(Copy, Clone, PartialEq, Eq, ArgEnum)]
+enum InputType {
+    Web,
+    YAML,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, ArgEnum)]
+enum OutputType {
+    #[clap(name = "gcal")]
+    GCal,
+    YAML,
+}
+
+#[derive(Parser)]
 #[clap(about, version, author)]
 struct Args {
+    #[clap(arg_enum, short, long, default_value="web")]
+    input: InputType,
+    #[clap(arg_enum, short, long, default_value="gcal")]
+    output: OutputType,
+
     #[clap(short, long, default_value="", env="SCMA_USERNAME")]
     username: String,
     #[clap(short, long, default_value="", env="SCMA_PASSWORD")]
@@ -35,21 +55,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args = Args::parse();
 
-    // let events = Web::new(&args.username, &args.password)
-    //     .read()
-    //     .await?;
+    let events = match args.input {
+        InputType::Web => {
+            Web::new(&args.username, &args.password)
+                .read()
+                .await?
+        }
+        InputType::YAML => {
+            info!(%args.file, "Loading events from file");
+            let events_yaml = std::fs::read_to_string(&args.file)?;
+            serde_yaml::from_str(&events_yaml)?
+        }
+    };
 
-    // println!("{}", serde_yaml::to_string(&events)?);
-
-    info!(%args.file, "Loading events from file");
-    let events = std::fs::read_to_string(&args.file)?;
-    let events: Vec<Event> = serde_yaml::from_str(&events)?;
-    info!("Querying Google Calendar");
-    GCal::new(
-        &args.calendar,
-        &args.client_secret_json_path,
-        &args.oauth_token_json_path,
-    ).write(&events).await?;
+    match args.output {
+        OutputType::GCal => {
+            info!("Querying Google Calendar");
+            GCal::new(
+                &args.calendar,
+                &args.client_secret_json_path,
+                &args.oauth_token_json_path,
+            ).write(&events).await?;
+        }
+        OutputType::YAML => {
+            println!("{}", serde_yaml::to_string(&events)?);
+        }
+    }
 
     Ok(())
 }
