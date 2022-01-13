b9f17f829688724d1768fcee1d9a40446c43ba3c
Rob Donnelly <rfdonnelly@gmail.com>
2022-01-12 21:20:24 -0800

    feat: use raw HTML descriptions
    instead of trying to convert to plaintext with intelligent newlines.
    Google Calendar supports HTML in event descriptions.
    It looks nicer and simplifies the implementation.
    
    Closes #5

--- src/input/web.rs
+++ src/input/web.rs
@@ -3,8 +3,6 @@ use crate::model::{Attendee, Comment, Event};
 use chrono::NaiveDate;
 use futures::{stream, StreamExt, TryStreamExt};
 use select::document::Document;
-use select::node::Data;
-use select::node::Node;
 use select::predicate::{And, Attr, Class, Name};
 use tap::prelude::*;
 use tracing::info;
@@ -240,7 +238,6 @@ impl TryFrom<(&str, Page)> for EventList {
 
         for event in &mut events {
             event.url = [base_url, &event.url].join("");
-            event.description = parse_description(&event.description);
         }
 
         Ok(Self(events))
@@ -262,52 +259,9 @@ impl FromIterator<Event> for EventList {
     }
 }
 
-fn parse_description(s: &str) -> String {
-    let document = Document::from(s);
-    let mut buffer = String::with_capacity(s.len());
-    let node = document.find(Name("body")).next().unwrap();
-    parse_node_text(&node, &mut buffer);
-    buffer
-}
-
-fn parse_node_text(node: &Node, buffer: &mut String) {
-    for child in node.children() {
-        match child.data() {
-            Data::Text(_) => {
-                let text = child.as_text().unwrap();
-                match text {
-                    // Ignore newline-only text elements
-                    "\n" => (),
-                    _ => buffer.push_str(text),
-                }
-            }
-            Data::Element(_, _) => {
-                // Handles case where we transition from a non-newline element to a newline element
-                // I.e. Inserts a newline between a non-newline element and a newline element
-                maybe_newline(&child, buffer);
-                parse_node_text(&child, buffer);
-                // Insert a newline at the end of a newline element
-                maybe_newline(&child, buffer);
-            }
-            Data::Comment(_) => (),
-        }
-    }
-}
-
-fn maybe_newline(node: &Node, buffer: &mut String) {
-    let buffer_ends_with_newline = buffer.chars().last().unwrap_or_default() == '\n';
-    let is_newline_element = matches!(node.name(), Some("p" | "div"));
-    let insert_newline = !buffer.is_empty() && !buffer_ends_with_newline && is_newline_element;
-    if insert_newline {
-        buffer.push('\n');
-    }
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
-
-    use indoc::indoc;
 
     use std::path::{Path, PathBuf};
 
@@ -351,44 +305,5 @@ mod test {
         let base_url = "https://www.rockclimbing.org";
         let events = EventList::try_from((base_url, page)).unwrap();
         insta::assert_yaml_snapshot!(events);
-    }
-
-    #[test]
-    fn parse_description_blank() {
-        let input = "";
-        let expected = "";
-        assert_eq!(parse_description(input), expected);
-    }
-
-    #[test]
-    fn parse_description_text() {
-        let input = "Trip Leader: Mike Sauter";
-        let expected = "Trip Leader: Mike Sauter";
-        assert_eq!(parse_description(input), expected);
-    }
-
-    #[test]
-    fn parse_description_basic_html() {
-        let input = "<p>Trip Leaders: Chao & C. Irving</p>\r\n<p>2 days of hard climbing in the Needles.  You should be a competent 5.9 climber to attend this outing as there are no easy routes here.  No kidding!</p>";
-        let expected = indoc! {"
-            Trip Leaders: Chao & C. Irving
-            2 days of hard climbing in the Needles.  You should be a competent 5.9 climber to attend this outing as there are no easy routes here.  No kidding!
-        "};
-        assert_eq!(parse_description(input), expected);
-    }
-
-    #[test]
-    fn parse_description_div() {
-        let input = "<font face=\"Arial, Verdana\"><span style=\"font-size: 13.3333px;\">Camping Fri and Sat nights at Joshua Tree, Ryan Campground.</span></font><div><font face=\"Arial, Verdana\"><div style=\"font-size: 13.3333px;\">Fri and Sat nights : Four campsites:</div><div style=\"font-size: 13.3333px;\"><span style=\"white-space:pre\">\t</span>#3 (2 parking spaces)</div><div style=\"font-size: 13.3333px;\"><span style=\"white-space:pre\">\t</span>#4 (2 parking spaces)</div><div style=\"font-size: 13.3333px;\"><span style=\"white-space:pre\">\t</span>#6 (2 parking spaces)</div><div style=\"font-size: 13.3333px;\"><span style=\"white-space: pre;\">\t</span>#7 (2 parking spaces)</div><div style=\"style\"><span style=\"font-size: 13.3333px;\">Trip Leader: Rob Donnelly</span></div></font></div>";
-        let expected = indoc! {"
-            Camping Fri and Sat nights at Joshua Tree, Ryan Campground.
-            Fri and Sat nights : Four campsites:
-            \t#3 (2 parking spaces)
-            \t#4 (2 parking spaces)
-            \t#6 (2 parking spaces)
-            \t#7 (2 parking spaces)
-            Trip Leader: Rob Donnelly
-        "};
-        assert_eq!(parse_description(input), expected);
     }
 }
